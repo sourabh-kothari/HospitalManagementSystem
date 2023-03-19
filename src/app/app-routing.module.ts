a4edc9a348078f85739a698c4574813aa1f82788
ankit3851 <ankitkumaransh6667@gmail.com>
2023-03-19 12:15:15 +0530

    routing ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdmindashComponent } from './admindash/admindash.component';
 import { AdminloginComponent } from './adminlogin/adminlogin.component';
-<<<<<<< HEAD
 import { BookappoinmetComponent } from './bookappoinmet/bookappoinmet.component';
-=======
-import { BookappointmentComponent } from './bookappointment/bookappointment.component';
->>>>>>> 7340812f7c9f0c655fa59e3b5ad2e634f6184172
 import { ContactusComponent } from './contactus/contactus.component';
 import { HomeComponent } from './home/home.component';
 import { LogincomponentComponent } from './logincomponent/logincomponent.component';
@@ -29,12 +25,8 @@ const routes: Routes = [
   {path:'register',component:RegisterComponent},
   {path:'updatedoc',component:UpdatedocComponent},
   {path:'patientdash',component:PatientdashComponent},
-<<<<<<< HEAD
   {path:'viewappoinments',component:ViewappoinmentsComponent},
   {path:'bookappoinment',component:BookappoinmetComponent}
-=======
-  {path:'bookappointment',component: BookappointmentComponent}
->>>>>>> 7340812f7c9f0c655fa59e3b5ad2e634f6184172
 ];
 
 @NgModule({
